b574009da7b8ce44b1e1d276d7ef75434d453928
jommari <johannes.kultanen@gmail.com>
2015-11-18 12:18:11 +0200

    Fixes title

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,11 +1,11 @@
 ###########################################################################
-#This game is made for practice the use of python.		  		  		  #
-#Rock, paper, scissors prompts player to 'choose his hand'.		  		  #
-#The computer chooses random hand and compares it to players hand.		  #
-#If the player wins the round, one point is added to players score.		  #
-#If the computer wins the round, one point is adds to the computers score.#
-#If the round ends with a draw, neither gets a point.					  #
-#game continues until chosen amount of rounds has been won.				  #
+#This game is made for practice the use of python.		  		  		  
+#Rock, paper, scissors prompts player to 'choose his hand'.		  		  
+#The computer chooses random hand and compares it to players hand.		  
+#If the player wins the round, one point is added to players score.		  
+#If the computer wins the round, one point is adds to the computers score.
+#If the round ends with a draw, neither gets a point.					  
+#game continues until chosen amount of rounds has been won.				  
 ###########################################################################
 
 #prompts the player for number of rounds
