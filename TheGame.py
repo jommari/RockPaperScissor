36e6b936a75fc256a5169e8a960caa3485b2fdef
jommari <johannes.kultanen@gmail.com>
2015-11-19 13:37:34 +0200

    File to play the game

---
+++ TheGame.py
@@ -0,0 +1,13 @@
+###########################################################################
+#RockPaperScissors game engine that uses functions from RockPaperScissor.py
+###########################################################################
+import RockPaperScissor
+
+#Ask how many rounds the player wants to player
+roundsLeft = RockPaperScissor.gameStart()
+
+#Loop through until 0 rounds are left
+While roundsLeft > 0:
+	
+	
+	
