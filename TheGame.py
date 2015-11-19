7d62ecd1db1389cc56c2dab1b6d633c48d68b85c
jommari <johannes.kultanen@gmail.com>
2015-11-19 15:29:30 +0200

    Playable but asks for range two times

--- TheGame.py
+++ TheGame.py
@@ -5,9 +5,31 @@ import RockPaperScissor
 
 #Ask how many rounds the player wants to player
 roundsLeft = RockPaperScissor.gameStart()
-
-#Loop through until 0 rounds are left
-While roundsLeft > 0:
+playerScore = 0
+computerScore = 0
+#Loop through until chosen rounds have been won
+while True: 
+	player = RockPaperScissor.playerHand()
+	computer = RockPaperScissor.computerHand()
 	
+	#'a' for rock, 'b' for paper and 'c' for scissors
+	if (player == 'a' and computer == 'b') or (player == 'b' and computer == 'c') or (player == 'c' and computer == 'a'):
+		print "The computer won this round!"
+		computerScore += 1
+		print "Player's score: ", playerScore
+		print "Computer's score: ", computerScore
+		if computerScore == roundsLeft:
+			print "The computer won %s rounds!" % (roundsLeft)
+			break
+	elif (player == 'a' and computer == 'c') or (player == 'c' and computer == 'b') or (player == 'b' and computer == 'a'):
+		print "The player won this round!"
+		playerScore += 1
+		print "Player's score: ", playerScore
+		print "Computer's score: ", computerScore
+		if playerScore == roundsLeft:
+			print "The player won %s rounds!" % (roundsLeft)
+			break
+	elif player == computer:
+		print "Draw\n"
 	
 	
